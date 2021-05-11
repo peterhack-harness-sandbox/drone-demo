88326e1b35ce1c65618f0708866b9694657b1084
Peter Hack <peterhack+github@gmail.com>
2021-05-10 23:34:02 -0400

    Update hello_test.go

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
     got := Hello()
-    want := "Hello"
+    want := "Hello, world"
 
     if got != want {
         t.Errorf("got %q want %q", got, want)
